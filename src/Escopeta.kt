eb810cc6262c95eeb84770ba89635b3ca2cb3b14
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-02-21 17:34:58 +0100

    cambios

---
+++ src/Escopeta.kt
@@ -0,0 +1,9 @@
+class Escopeta(numbalas:Int,numbalascargadas: Int){
+
+
+    fun disparo(tambor:MutableList<Int>, i:Int): Boolean{
+
+    }
+
+
+}
---
+++ src/Tambor.kt
@@ -0,0 +1,13 @@
+import kotlin.random.Random
+
+class Tambor() {
+
+    fun tamanotambor():Int{
+        return Random.nextInt(1,9)
+    }
+
+    fun cagartambor(numebala: Int) : MutableList<Int>{
+
+    }
+
+}
